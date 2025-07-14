f93957c90802d2abba8372b85b3602b1855c7fa4
Kauanzilla <kauan.jagger@gmail.com>
2025-07-14 14:58:37 -0300

    automatizei algumas funções e arrumei o html/css de algumas partes

--- comandos.js
+++ comandos.js
@@ -1,54 +1,160 @@
 var champions
 
-    async function buscarChampions() {
+async function buscarChampions() {
+    
+    const lista = document.getElementById("lengthchamps")
+    const response = await fetch("http://localhost:8080/api/champions")
 
-        const lista = document.getElementById("lengthchamps")
-        const response = await fetch("http://localhost:8080/api/champions")
+    lista.innerHTML = ""
 
-        const data = await response.json()
-        champions = data
+    const data = await response.json()
+    champions = data
 
-        data.forEach(champion => {
-            lista.innerHTML += `<div class="mouse" onclick="searchIdRef(${champion.id})" style="border: 2px solid black; padding: 12px 0; width: 100%; text-indent: 10px;
-             background-image: linear-gradient(to right, black, transparent), url('${champion.imageUrl}'); background-size: 100%;
-              background-position: center; color: red; font-weight: bold;">${champion.id} - ${champion.name}</div>`
+    data.forEach(champion => {
+        lista.innerHTML += `<div class="mouse" onclick="searchIdRef(${champion.id})" style="border: 2px solid black; padding: 12px 0;
+             width: 100%; text-indent: 10px; background-image: linear-gradient(to right, black, transparent), url('${champion.imageUrl}'); 
+             background-size: 100%; background-position: center; color: red; font-weight: bold;">${champion.id} - ${champion.name}</div>`
+    })
+
+}
+
+async function searchId() {
+
+    var id = document.getElementById("idSearch").value
+
+    juncao(id)
+
+}
+
+async function searchIdRef(id) {
+
+    juncao(id)
+
+
+}
+
+function testimg() {
+    
+    var img = document.getElementById("cimg")
+    var input = document.getElementById("curl")
+
+    img.src = input.value
+
+    input.value = ""
+}
+
+async function juncao(id) {
+
+    var imagem = document.getElementById("imagesrc")
+    var namebigger = document.getElementById("champname")
+    var deleteBtn = document.getElementById("delete")
+    var code = document.getElementById("code")
+    const response = await fetch(`http://localhost:8080/api/champions/${id}`)
+    const data = await response.json()
+
+    imagem.src = data.imageUrl
+    namebigger.textContent = data.name
+    deleteBtn.style.visibility = "visible"
+    code.innerHTML = data.id
+    table.innerHTML = `<tbody>
+                        <tr>
+                            <td class="tdNome">HP</td>
+                            <td class="tdValor">${data.hp}</td>
+                        </tr>
+                        <tr>
+                            <td class="tdNome">MP</td>
+                            <td class="tdValor">${data.mp}</td>
+                        </tr>
+                        <tr>
+                            <td class="tdNome">Armor</td>
+                            <td class="tdValor">${data.armor}</td>
+                        </tr>
+                        <tr>
+                            <td class="tdNome">Move Speed</td>
+                            <td class="tdValor">${data.moveSpeed}</td>
+                        </tr>
+                        <tr>
+                            <td class="tdNome">Attack Damage</td>
+                            <td class="tdValor">${data.attackDamage}</td>
+                        </tr>
+                        <tr>
+                            <td class="tdNome">Attack Range</td>
+                            <td class="tdValor">${data.attackRange}</td>
+                        </tr>
+                    </tbody>`
+
+}
+
+async function send() {
+    
+    var hpInput = document.getElementById("chp")
+    var mpInput = document.getElementById("cmp")
+    var armorInput = document.getElementById("carmor")
+    var speedInput = document.getElementById("cmvspd")
+    var damageInput = document.getElementById("cattdmg")
+    var rangeInput = document.getElementById("cattrg")
+    var nameInput = document.getElementById("cname")
+    var imgInput = document.getElementById("cimg")
+    var titleInput = document.getElementById("ctitle")
+    var descriptionInput = document.getElementById("cdescrip")
+    var tagsInput = document.getElementById("ctag")
+
+    const response = await fetch("http://localhost:8080/api/champions", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            title: titleInput.value,
+            description: descriptionInput.value,
+            imageUrl: imgInput.src,
+            tags: [
+                tagsInput.value
+            ],
+            hp: hpInput.value,
+            mp: mpInput.value,
+            moveSpeed: speedInput.value,
+            armor: armorInput.value,
+            attackRange: rangeInput.value,
+            attackDamage: damageInput.value
         })
+    })
 
-    }
+    const id = await response.text()
 
-    async function searchId() {
+    juncao(id)
 
-        var id = document.getElementById("idSearch").value
-        var imagem = document.getElementById("imagesrc")
-        var hp = document.getElementById("hp/mp")
-        var speed = document.getElementById("speed")
-        var damage = document.getElementById("damage")
-        var namebigger = document.getElementById("champname")
-        const response = await fetch(`http://localhost:8080/api/champions/${id}`)
-        const data = await response.json()
+    buscarChampions()
 
-        imagem.src = data.imageUrl
-        hp.innerHTML = `Hp: ${data.hp} Mp: ${data.mp}`
-        speed.innerHTML = `Move Speed: ${data.moveSpeed} Armor: ${data.armor}`
-        damage.innerHTML = `Attack Damage: ${data.attackDamage} Attack Range: ${data.attackRange}`
-        namebigger.textContent = data.name
+    alert(`${name} created successfuly!!`)
 
-    }
+    hpInput.value = ""
+    mpInput.value = ""
+    armorInput.value = ""
+    speedInput.value = ""
+    damageInput.value = ""
+    rangeInput.value = ""
+    nameInput.value = ""
+    imgInput.src = ""
+    titleInput.value = ""
+    descriptionInput.value = ""
+    tagsInput.value = ""
+}
 
-        async function searchIdRef(id) {
+ async function deleteById(){
 
-        var hp = document.getElementById("hp/mp")
-        var speed = document.getElementById("speed")
-        var damage = document.getElementById("damage")    
-        var imagem = document.getElementById("imagesrc")
-        var namebigger = document.getElementById("champname")
-        const response = await fetch(`http://localhost:8080/api/champions/${id}`)
-        const data = await response.json()
+    var id = document.getElementById("code").textContent
 
-        imagem.src = data.imageUrl
-        hp.innerHTML = `Hp: ${data.hp} Mp: ${data.mp}`
-        speed.innerHTML = `Move Speed: ${data.moveSpeed} Armor: ${data.armor}`
-        damage.innerHTML = `Attack Damage: ${data.attackDamage} Attack Range: ${data.attackRange}`
-        namebigger.textContent = data.name
+    const response = await fetch(`http://localhost:8080/api/champions/${id}`, {
+        method: "DELETE"
+    })
 
-    }
+    location.reload()
+}
+
+
+
+
+
+
